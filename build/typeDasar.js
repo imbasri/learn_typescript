d87155fbd2b481d7584baf277167014c850bfead
imbasri <imbasri.dev@gmail.com>
2023-02-24 19:08:54 +0700

    type Dasar

---
+++ build/typeDasar.js
@@ -0,0 +1,19 @@
+"use strict";
+console.log("hello");
+// static type
+// string
+let hewan; // cara 1
+let makanan = "bakso"; // cara 2
+hewan = "kambing";
+// hewan = 20; Type 'number' is not assignable to type 'string'.
+let nama = "kucing";
+nama = "capybara";
+// nama = 20; ini akan error karena nama seharus ya type string
+// number
+let umur;
+umur = 26;
+umur = 30;
+// boolean
+let isMarried;
+isMarried = true;
+isMarried = false;
---
+++ src/typeDasar.ts
@@ -0,0 +1,20 @@
+console.log("hello");
+// static type
+
+// string
+let hewan: string; // cara 1
+let makanan: string = "bakso"; // cara 2
+hewan = "kambing";
+// hewan = 20; Type 'number' is not assignable to type 'string'.
+let nama = "kucing";
+nama = "capybara";
+// nama = 20; ini akan error karena nama seharus ya type string
+
+// number
+let umur: number;
+umur = 26;
+umur = 30;
+// boolean
+let isMarried: boolean;
+isMarried = true;
+isMarried = false;
